179a8d1445b6f39a1c6f4fdb48d19f1a54fa3e1b
dspeckman76 <d.speckman76@gmail.com>
2025-12-14 10:16:33 -0600

    Dec 14 Update

---
+++ script.js
@@ -0,0 +1,49 @@
+let isModalOpen = false;
+let contrastToggle = false;
+
+
+
+function toggleContrast() {
+    contrastToggle = !contrastToggle;
+    if (contrastToggle) {
+      document.body.classList += " dark-theme"
+    }
+    else {
+      document.body.classList.remove("dark-theme")
+    }
+  }
+
+
+function contact(event) {
+    event.preventDefault();
+    const loading = document.querySelector(".modal__overlay--loading");
+    const success = document.querySelector(".modal__overlay--success");
+    loading.classList += " modal__overlay--visible";
+    emailjs
+      .sendForm(
+        "service_kttwbwb",
+        "template_tcymvqp",
+        event.target,
+        "_x3qbT2wZd8xIkbEf"
+      )
+      .then(() => {
+        loading.classList.remove("modal__overlay--visible");
+        success.classList += " modal__overlay--visible";
+      })
+      .catch(() => {
+        loading.classList.remove("modal__overlay--visible");
+        alert(
+          "The email service is temporarily unavailable. Please contact me directly at d.speckman76@gmail.com"
+        );
+      });
+  }
+  
+  
+  function toggleModal() {
+    if (isModalOpen) {
+      isModalOpen = false;
+      return document.body.classList.remove("modal--open");
+    }
+    isModalOpen = true;
+    document.body.classList += " modal--open";
+  }
